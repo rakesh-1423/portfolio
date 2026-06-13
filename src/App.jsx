2e0ee4f03a13667725b2c33c0f99befbdf19ba1e
rakesh-1423 <rakesh06264@gmail.com>
2026-06-13 08:46:34 +0530

    Comment show on the top so currect comment issue

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,13 @@ import Blurblob from "./Blurblob";
 function App() {
   return (
     <div className="bg-[#050414]">
-      // This is use for the blurBlob place near above about
+      {/* This is use for the blurBlob place near above about */}
       <Blurblob
         position={{ top: "35%", left: "20%" }}
         size={{ width: "30%", height: "40%" }}
       />
 
-      // This is for grid lines design on about above and just below navbar
+      {/*  This is for grid lines design on about above and just below navbar */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
 
       <div className="relative pt-20 text-amber-50">
